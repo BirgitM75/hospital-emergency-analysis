6abafc6d3bd6496dfd029ec7fc56d95312863fb2
BirgitM75 <birgitmdrews@gmail.com>
2026-08-13 19:36:53 +0200

    Create analysis.sql

---
+++ analysis.sql
@@ -0,0 +1,3 @@
+# Patient Triaging & Non-Urgent Cases'
+Question: What is the percentage of patients arriving at the emergency department who are classified as non-urgent cases?
+-- Calculates the percentage of patients per urgency level
